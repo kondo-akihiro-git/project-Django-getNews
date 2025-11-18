af56ef7541fd521c5517b0fd86b0e21680ad8358
kondo-akihiro-git <2000akisan@gmail.com>
2025-11-18 20:20:07 +0900

    初回コミット

---
+++ api/views.py
@@ -0,0 +1,7 @@
+# api/views.py
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['GET'])
+def hello(request):
+    return Response({"message": "hello"})
