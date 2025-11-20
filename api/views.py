b59fbe829d5693aa752339f632b0d30638a8c8f9
kondo-akihiro-git <2000akisan@gmail.com>
2025-11-20 20:55:22 +0900

    Gemini連携開始

--- api/views.py
+++ api/views.py
@@ -2,6 +2,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from api.service.getNews import fetch_news
+from google import genai
 
 
 @api_view(['GET'])
@@ -11,6 +12,15 @@ def get_news(request):
     """
     try:
         articles = fetch_news()
+
+        client = genai.Client()
+
+        response = client.models.generate_content(
+        model="gemini-2.5-flash", contents="日本語で最新のニュースを教えてください")
+        return Response({
+            "output_text": response.text
+        })
+        
     except RuntimeError as e:
         return Response(
             {"detail": str(e)},
@@ -22,6 +32,7 @@ def get_news(request):
             {"detail": "RSSフィードに記事が見つかりませんでした"},
             status=status.HTTP_502_BAD_GATEWAY
         )
+    
 
     return Response({
         "count": len(articles),
